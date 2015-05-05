f5eb20fa9e46a0d6cde9b7e6b2fcc9a2a3237ab1
Derek <derekr@me.com>
2015-05-05 11:47:26 -0400

    Favicon and initial about page

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'Tab Champ', user: req.user });
+});
+
 router.get('/', function(req, res, next) {
   res.render('leaderboard', { title: 'Leader Board', user: req.user });
 });
+
 
 router.get('/leaderboard', function(req, res, next) {
   res.render('leaderboard', { title: 'Leader Board', user: req.user });
