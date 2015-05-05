9c6ed81ff9c4a83704a5d1ce4ebc29ba57b6299c
Derek <derekr@me.com>
2015-05-05 08:18:45 -0400

    Adds signin button

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', user: req.user });
+  res.render('leaderboard', { title: 'Leader Board', user: req.user });
 });
 
 router.get('/leaderboard', function(req, res, next) {
